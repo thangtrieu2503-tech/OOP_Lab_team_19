5e31bb253e52bf841a22157b4b4e63926c0730fa
lhp2006-git <ple690391@gmail.com>
2026-06-17 14:15:29 +0700

    update

--- src/VehicleSystem/vehicle/type/Bus.java
+++ src/VehicleSystem/vehicle/type/Bus.java
@@ -6,6 +6,6 @@ import VehicleSystem.behavior.NormalBehavior;
 public class Bus extends Vehicle {
     public Bus(double startX, double startY, int i, int i1, double v, NormalBehavior normalBehavior) {
         // Xác xe to dài: rộng 30, dài 90, tốc độ rùa bò 3.5
-        super(startX, startY, 20.0, 60.0, 1.2, new NormalBehavior());
+        super(startX, startY, 20.0, 60.0, 1.0, new NormalBehavior());
     }
 }
--- src/VehicleSystem/vehicle/type/Motorbike.java
+++ src/VehicleSystem/vehicle/type/Motorbike.java
@@ -6,6 +6,6 @@ import VehicleSystem.behavior.NormalBehavior;
 public class Motorbike extends Vehicle {
     public Motorbike(double startX, double startY, int i, int i1, double v, NormalBehavior normalBehavior) {
         // Nhỏ gọn: rộng 12, dài 25, phóng nhanh 6.0, não trẻ trâu bốc lửa
-        super(startX, startY, 7.0, 18.0, 0.8, new NormalBehavior());
+        super(startX, startY, 7.0, 18.0, 0.7, new NormalBehavior());
     }
 }
